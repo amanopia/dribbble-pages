a6f834044efc8c4ee51f64edc4ca4e926cc72448
Aman Verma <amanvermaspace@gmail.com>
2023-10-16 13:29:13 +0530

    shortened the code

--- site1/js/index.js
+++ site1/js/index.js
@@ -5,52 +5,24 @@ const para1 = document.querySelector('.para1');
 const para2 = document.querySelector('.para2');
 const para3 = document.querySelector('.para3');
 const paras = document.querySelectorAll('.para');
-
+console.log(paras)
 
 let animDel = 0.1;
-// PARA1 ANIMATION
-(
-    function(){
-        let element = para1.children;
-        for(let i = 0; i<element.length; i++){
-            element[i].style.transition = "all 0.4s ease";
-            element[i].style.opacity = "0";
-            element[i].style.transitionDelay = animDel+"s";
-            element[i].style.transform = "translateY(2rem)"
-            animDel += 0.1;
+function addAnimation(el){
+    let element = el.children;
+    for(let i = 0; i<element.length; i++){
+        element[i].style.transition = "all 0.4s ease";
+        element[i].style.opacity = "0";
+        element[i].style.transitionDelay = animDel+"s";
+        element[i].style.transform = "translateY(2rem)"
+        animDel += 0.1;
     }
-    }
-)();
+}
 
-// PARA2 ANIMATION
-(
-    function(){
-        let element = para2.children;
-        for(let i = 0; i<element.length; i++){
-            element[i].style.transition = "all 0.4s ease";
-            element[i].style.opacity = 0;
-            element[i].style.transitionDelay = animDel + "s";
-            element[i].style.transform = "translateY(2rem)"
-            animDel += 0.1;
-            
-        }
-    }
+paras.forEach((element) => {
+    addAnimation(element);
+})
 
-)();
-
-// PARA3 ANIMATION
-(
-    function(){
-        let element = para3.children;
-        for(let i = 0; i<element.length; i++){
-            element[i].style.transition = "all 0.4s ease";
-            element[i].style.opacity = 0;
-            element[i].style.transitionDelay = animDel + "s";
-            element[i].style.transform = "translateY(2rem)";
-            animDel += 0.1;
-        }
-    }
-)();
 (function(){
     navigation.style.overflowY = "hidden";
     navigationContent.style.transform = "translateY(5rem)";
@@ -72,18 +44,13 @@ revealAfter2Sec();
 
 function animateHeadings(){
     setTimeout(() => {
-        for(let i = 0; i<para1.children.length; i++){
-            para1.children[i].style.opacity = "1";
-            para1.children[i].style.transform = "translateY(0rem)";
-        }
-        for(let j = 0;j<para2.children.length; j++){
-            para2.children[j].style.opacity = "1";
-            para2.children[j].style.transform = "translateY(0rem)";
-        }
-        for(let j = 0;j<para3.children.length; j++){
-            para3.children[j].style.opacity = "1";
-            para3.children[j].style.transform = "translateY(0rem)";
-        }
+        para2.forEach((el) => {
+            const element = el.children;
+            for(let i = 0 ;i < element.length; i++){
+                element[i].style.opacity = 1;
+                element[i].style.transform = "translateY(0rem)";
+            }
+        })
     }, 2000)
 }
 

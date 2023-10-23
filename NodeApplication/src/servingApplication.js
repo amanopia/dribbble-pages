1d0d555ee379dabbb1e3b7cdf264d4fcd6d285db
Aman Verma <amanvermaspace@gmail.com>
2023-10-23 12:35:12 +0530

    configured server and added html pages

--- NodeApplication/src/servingApplication.js
+++ NodeApplication/src/servingApplication.js
@@ -1,29 +1,47 @@
+// node imports
+import path from 'path';
+import { fileURLToPath } from 'url';
+import hbs from 'hbs';
+// npm imports
 import express from 'express';
 
 const app = express();
 
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../public")))
+app.set('view engine', 'hbs');
+
+
+const viewsPath = path.join(__dirname, "../templates/views");
+app.set('views', viewsPath);
+hbs.registerPartials(path.join(__dirname, "../templates/partials"))
+
 //! ROUTES
-app.get('/', (req, res)=>{
-    res.send({
-        name: "jOHN",
-        age: 45
-    });
+app.get('', (req, res)=>{
+    res.render('index');
 })
 
 app.get('/about', (req, res) => {
-
+    res.render('about');
 })
 
 app.get('/services', (req, res) => {
-
+    res.render('services');
 })
 
 app.get('/contact-us', (req, res) => {
-    res.send('<h1>Hola</h1>')
+    res.render('contact-us');
 })
 
-
+app.get("*", (req, res) => {
+    res.render("404", {
+        src: "/assets/404.jpg",
+        alt: "404 image"
+    });
+})
 const port = 3002;
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);

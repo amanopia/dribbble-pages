a91b08afd513b34c09346bd5f086a791861379bc
Aman Verma <amanvermaspace@gmail.com>
2023-11-26 09:27:49 +0530

    added indentation

--- NodeApplication/public/js/offer.js
+++ NodeApplication/public/js/offer.js
@@ -2,82 +2,91 @@ serviceAnimationDelay = 0.1;
 
 //! ------------------ UPPER CONTENT ------------------
 
-const offerUpperContentChildren = [...document.querySelector('.offer-content-upper').children];
+const offerUpperContentChildren = [
+  ...document.querySelector(".offer-content-upper").children,
+];
 console.log(offerUpperContentChildren);
-const offerUpperContent = document.querySelector('.offer-content-upper');
+const offerUpperContent = document.querySelector(".offer-content-upper");
 
 // UPPER CONTENT -- INITIAAL STYLINGS
-for(let i = 0; i<offerUpperContentChildren.length; i++){
-    offerUpperContentChildren[i].classList.add('hidden');
-    offerUpperContentChildren[i].style.transition = "all 0.4s ease";
-    offerUpperContentChildren[i].style.transitionDelay = serviceAnimationDelay + "s";
-    serviceAnimationDelay += 0.1;
+for (let i = 0; i < offerUpperContentChildren.length; i++) {
+  offerUpperContentChildren[i].classList.add("hidden");
+  offerUpperContentChildren[i].style.transition = "all 0.4s ease";
+  offerUpperContentChildren[i].style.transitionDelay =
+    serviceAnimationDelay + "s";
+  serviceAnimationDelay += 0.1;
 }
 // IMAGE -- OBSERVER FUNCTION
-const offerUpperContentObserver = new IntersectionObserver((entries) => {
+const offerUpperContentObserver = new IntersectionObserver(
+  (entries) => {
     entries.forEach((entry) => {
-        // entry.target.classList.toggle('hidden', entry.isIntersecting); // this adds the hidden class when element.isIntersecting evaluates to true
+      // entry.target.classList.toggle('hidden', entry.isIntersecting); // this adds the hidden class when element.isIntersecting evaluates to true
 
-        if(entry.isIntersecting){
-            setTimeout(() => {
-                entry.target.classList.toggle('hidden', false);
-                observer.unobserve(entry.target);
-            }, timeOutCount)
-
-        }
-    })
-}, {
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          entry.target.classList.toggle("hidden", false);
+          observer.unobserve(entry.target);
+        }, timeOutCount);
+      }
+    });
+  },
+  {
     threshold: 1,
-})
+  }
+);
 
 offerUpperContentChildren.forEach((children) => {
-    // console.log(children);
-    offerUpperContentObserver.observe(children);
-})
+  // console.log(children);
+  offerUpperContentObserver.observe(children);
+});
 
 //! ------------------ BOTTOM SECTION ------------------
-const offerBottomSectionChildren = [...document.querySelector('.offers').children];
-const offerBottomParentContainer = document.querySelector('.offers');
+const offerBottomSectionChildren = [
+  ...document.querySelector(".offers").children,
+];
+const offerBottomParentContainer = document.querySelector(".offers");
 
-for(const element of offerBottomSectionChildren){
-    element.classList.add('hide');
-    element.style.transition = "all 0.4s cubic-bezier(.7,-0.02,.73,1.01)"
-    element.style.transitionDelay = serviceAnimationDelay + "s";
-    
-    // Adding animation to inner elements
-    const childArr = [...element.children]
-    //? setting a new delay duration for inner elements
-    //? Delayed the inner duration by 0.5s which is enough for the outer box animation to load and the inner to start executing
-    let delayDuration = 0.5;
-    for(let j = 0; j<childArr.length; j++){
-        // console.log(childArr[j]);
-        childArr[j].classList.add('hidden');
-        childArr[j].style.transition = 'all 0.4s ease';
-        childArr[j].style.transitionDelay = serviceAnimationDelay + delayDuration + "s";
-        //? incrementing the inner delay duration by 0.1
-        delayDuration += 0.1
-    }
-    serviceAnimationDelay += 0.3;
+for (const element of offerBottomSectionChildren) {
+  element.classList.add("hide");
+  element.style.transition = "all 0.4s cubic-bezier(.7,-0.02,.73,1.01)";
+  element.style.transitionDelay = serviceAnimationDelay + "s";
+
+  // Adding animation to inner elements
+  const childArr = [...element.children];
+  //? setting a new delay duration for inner elements
+  //? Delayed the inner duration by 0.5s which is enough for the outer box animation to load and the inner to start executing
+  let delayDuration = 0.5;
+  for (let j = 0; j < childArr.length; j++) {
+    // console.log(childArr[j]);
+    childArr[j].classList.add("hidden");
+    childArr[j].style.transition = "all 0.4s ease";
+    childArr[j].style.transitionDelay =
+      serviceAnimationDelay + delayDuration + "s";
+    //? incrementing the inner delay duration by 0.1
+    delayDuration += 0.1;
+  }
+  serviceAnimationDelay += 0.3;
 }
 
-const bottomOfferContainerObserver = new IntersectionObserver((entries) => {
+const bottomOfferContainerObserver = new IntersectionObserver(
+  (entries) => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            console.log("offers is intersecting");
-            offerBottomSectionChildren.forEach((child) => {
-                child.classList.remove('hide', false);
-                
-                const contentChildren = [...child.children];
-                contentChildren.forEach((contentChild) => {
-                    contentChild.classList.remove('hidden');
-                })
-            } )
-        }
-    })
-}, {
-    threshold: 0.3
-})
+      if (entry.isIntersecting) {
+        console.log("offers is intersecting");
+        offerBottomSectionChildren.forEach((child) => {
+          child.classList.remove("hide", false);
+
+          const contentChildren = [...child.children];
+          contentChildren.forEach((contentChild) => {
+            contentChild.classList.remove("hidden");
+          });
+        });
+      }
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
 
 bottomOfferContainerObserver.observe(offerBottomParentContainer);
-
-

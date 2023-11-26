955ea0b5938ad88e582ac709a9dfcfc62ded7b47
Aman Verma <amanvermaspace@gmail.com>
2023-11-26 20:35:51 +0530

    finished the about section styling

--- NodeApplication/src/servingApplication.js
+++ NodeApplication/src/servingApplication.js
@@ -1,55 +1,55 @@
 // node imports
-import path from 'path';
-import { fileURLToPath } from 'url';
-import hbs from 'hbs';
+import path from "path";
+import { fileURLToPath } from "url";
+import hbs from "hbs";
 // npm imports
-import express from 'express';
-import anime from 'animejs';
-
+import express from "express";
+import anime from "animejs";
 
 const app = express();
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "../public")))
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.set('view engine', 'hbs');
-
+app.set("view engine", "hbs");
 
 const viewsPath = path.join(__dirname, "../templates/views");
-app.set('views', viewsPath);
-hbs.registerPartials(path.join(__dirname, "../templates/partials"))
+app.set("views", viewsPath);
+hbs.registerPartials(path.join(__dirname, "../templates/partials"));
 
 //! ROUTES
-app.get('', (req, res)=>{
-    res.render('index', {
-        img: "/assets/star.png",
-        alt : "img"
-    });
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    img: "/assets/star.png",
+    alt: "img",
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about');
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    img1: "/assets/arrow.png",
+    img2: "/assets/asterisk.png",
+    video: "/assets/video.mp4",
+  });
+});
 
-app.get('/services', (req, res) => {
-    res.render('services');
-})
+app.get("/services", (req, res) => {
+  res.render("services");
+});
 
-app.get('/contact-us', (req, res) => {
-    res.render('contact-us');
-})
+app.get("/contact-us", (req, res) => {
+  res.render("contact-us");
+});
 
 app.get("*", (req, res) => {
-    res.render("404", {
-        src: "/assets/404.jpg",
-        alt: "404 image"
-    });
-})
+  res.render("404", {
+    src: "/assets/404.jpg",
+    alt: "404 image",
+  });
+});
 const port = 3002;
-app.listen(port, ()=>{
-    console.log(`Listening on port ${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
